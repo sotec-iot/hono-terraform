4645c04c0263678452f4d2e6c950a73fe4842c1e
matthiask <m.kaemmer@sotec.eu>
2023-04-18 18:39:29 +0200

    Add environment-template and readme

---
+++ environment-template/infrastructure/main.tf
@@ -0,0 +1,7 @@
+module "infrastructure" {
+  source = "git@github.com:sotec-iot/hono-extras.git//terraform/infrastructure?ref=terraform"
+  
+  project_id     = local.project_id
+  region         = local.region
+  node_locations = local.node_locations
+}
